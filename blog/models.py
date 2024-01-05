9964f399d8641c609df24b64f807b8c3e80e4740
Weakne55 <karpovich.bv@phystech.edu>
2024-01-05 08:50:42 +0000

    change start page and titles to title in model Post

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	titles = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
@@ -14,5 +14,5 @@ class Post(models.Model):
 		self.save()
 	
 	def __str__(self):
-		return self.titles
+		return self.title
 # Create your models here.
--- bside/urls.py
+++ bside/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
 	path('',include('homepage.urls')),
     path('admin/', admin.site.urls),
+	path('blog/', include('blog.urls')),
 ]
